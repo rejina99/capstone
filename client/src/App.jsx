77c72a4e5ae5d17fd23bf65640baf4dd88ace918
Rejina Maharjan <mhrzn.rezyna07@gmail.com>
2024-01-30 19:33:12 -0500

    Initial Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,20 @@
-import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import About from './pages/About';
+
 export default function App(){
-  return <h1 className='text-red'>App</h1>
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/login" element={<Login />} />    
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+
+
+  </Routes>
+  </BrowserRouter>
 }
