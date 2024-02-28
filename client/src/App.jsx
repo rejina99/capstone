a406f95cbe27e3759f12e2a57bf2ff2e8220cc21
sunil2619 <84694591+sunil2619@users.noreply.github.com>
2024-02-28 13:40:45 -0500

    working on home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import React from 'react'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Header from './components/Header'
+
+
+
 
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+
+    <BrowserRouter>
+
+
+      <Header />
+
+
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+
+      </Routes>
+
+    </BrowserRouter>
   )
 }
